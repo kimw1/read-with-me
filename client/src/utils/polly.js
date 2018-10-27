1ddcbef42fd81c1ef958354f9fdcca54e1a54c02
sammzz10 <johnkim3188@gmail.com>
2018-10-27 14:06:13 -0700

    small change to ignore and polly

--- client/src/utils/polly.js
+++ client/src/utils/polly.js
@@ -17,11 +17,11 @@ class PollyContainer extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-                OutputFormat: "mp3",
-                Text: "",
-                TextType: "text",
-                VoiceId: "",
-                url: null
+            OutputFormat: "mp3",
+            Text: "",
+            TextType: "text",
+            VoiceId: "",
+            url: null
         }
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -30,7 +30,7 @@ class PollyContainer extends React.Component {
     loadLibrary = () => {
         API.getLibrary()
             .then(res => this.setState({
-                text: this.res.data
+                text: res.data
             }))
             .catch(err => console.log(err));
     };
@@ -47,7 +47,7 @@ class PollyContainer extends React.Component {
     }
 
     handleChange(event) {
-        const {name, value} = event.target
+        const { name, value } = event.target
         this.setState({
             [name]: value
         });
@@ -55,57 +55,61 @@ class PollyContainer extends React.Component {
 
     handleSubmit(event) {
         event.preventDefault();
-        
+
         if (this.state.Text !== "") {
             this.setState({
-              OutputFormat: "mp3",
+                OutputFormat: "mp3",
                 TextType: "text",
                 Text: this.state.Text,
                 VoiceId: this.state.VoiceId
-            }, this.handleTextToVoice )
+            }, this.handleTextToVoice)
             console.log(this.state);
-
-                // .then(API.saveItem({ Text: this.state.Text }))
-                // .then(this.setState({ Text: "" }));
+            //.then(API.saveItem({ Text: this.state.Text }))
+            //this.setState({ Text: "", VoiceId: "" });
         };
     };
 
-    // handleVoices = (props) => {
-
-    //     AWS.polly.describeVoices(this.state, (err, data) => {
-    //         if (err) {
-    //             console.log(err, err.stack);
-    //         }
-    //         else console.log(data);
-
-
-    //     });
-    // }
-
     handleTextToVoice = () => {
-        const {url, ...params} = this.state;
+        const { url, ...params } = this.state;
         console.log(this.state);
         const polly = new AWS.Polly.Presigner();
-        polly.getSynthesizeSpeechUrl(params, [60*60*24*7], (error, url) => {
+        polly.getSynthesizeSpeechUrl(params, [60 * 60 * 24 * 7], (error, url) => {
             console.log(this.state);
             if (error) {
                 console.log(error.code, error.stack, error)
             }
             console.log("This is your data " + url);
+<<<<<<< HEAD
             this.setState({url});
+=======
+            this.setState({ url });
+>>>>>>> small change to ignore and polly
             let pollyPlayer = document.getElementById("polly-audio");
             pollyPlayer.load();
         });
     };
 
+    // cancelButton = () => {
+    //     this.setState({
+    //         OutputFormat: "mp3",
+    //         Text: "",
+    //         TextType: "text",
+    //         VoiceId: "",
+    //         url: null})
+    // }
 
+    clearForm = () => {
+        document.getElementById("clear")[0].reset();
+    }
 
     render() {
         return (
             <div>
-                <form>
-                    <label>Please input text you would like spoken.
+                <form name="Input-text-to-read" id="clear">
+                    <div>
+                        <label>Please input text you would like spoken.
                     <textarea name="Text" defaultValue={this.state.Text} onChange={this.handleChange} className="form-control" />
+<<<<<<< HEAD
                     </label>
                     <input type="submit" disabled={this.state.Text === ''} onClick={this.handleSubmit} value="Submit" className="btn btn-info" />
                 </form>
@@ -116,12 +120,25 @@ class PollyContainer extends React.Component {
                         {/* <p>Your browser doesn't support HTML5 audio. Here is a <a href={url}>link to the audio</a> instead.</p> */}
                     </audio>
                     <div>Please choose a language.</div>
+=======
+                        </label>
+                        <input type="submit" disabled={this.state.Text === ''} onClick={this.handleSubmit} value="Submit" className="btn btn-info" />
+                        <input type="submit" disabled={this.state.Text === ''} onClick={this.clearForm} value="Clear" className="btn btn-info" />
+                    </div>
+                    <div>
+                        <audio controls id="polly-audio">
+                            <source src={this.state.url} type="audio/mp3" />
+                            <p>Your browser doesn't support HTML5 audio. Here is a <a href={this.state.url}>link to the audio</a> instead.</p>
+                        </audio>
+                        <div>Please choose a language.</div>
+>>>>>>> small change to ignore and polly
                         <select onChange={this.handleChange} name="VoiceId">
-                        <option>---</option>
-                        <option className="english"  value="Joanna">US English</option>
-                        <option className="french"  value="Celine">French</option>
-                    </select>
-                </div>
+                            <option>---</option>
+                            <option className="english" value="Joanna">US English</option>
+                            <option className="french" value="Celine">French</option>
+                        </select>
+                    </div>
+                </form>
             </div>)
     };
 };
