6afd0887cbedf289b450b469fba309bb953efab3
Jae <jjyhong123@gmail.com>
2018-10-29 02:08:03 -0700

    added alt info

--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -8,14 +8,14 @@ import img3 from "../../utils/images/card-img-3.png";
 const Card = () => (
     <div className="card-deck">
         <div className="card">
-            <img className="card-img-top" src={img1} alt="Card image cap" />
+            <img className="card-img-top" src={img1} alt="Book icon" />
             <div className="card-body">
                 <h3 className="card-title">Image-to-voice</h3>
                 <p className="card-text">Designed with the visually impaired in mind. Simply take a picture of your surroundings and our app will narrate whatever text is present.</p>
             </div>
         </div>
         <div className="card">
-            <img className="card-img-top" src={img2} alt="Card image cap" />
+            <img className="card-img-top" src={img2} alt="Icon of pdf file" />
             <div className="card-body">
                 <h3 className="card-title">Text-to-voice</h3>
                 <p className="card-text">Make your morning commute more productive. RWM will accept a document in one of the supported text formats and read it to you.</p>
